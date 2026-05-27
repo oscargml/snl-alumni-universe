e7e33d70f7571c6fb8432097d9d9bb2d98823a43
oscargml <oscargml@gmail.com>
2026-05-27 16:53:20 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,23 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+<body className="min-h-full flex flex-col">
+  {children}
+
+  <Script
+    src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"
+    strategy="afterInteractive"
+  />
+  <Script id="google-analytics" strategy="afterInteractive">
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-33W8GG195N');
+    `}
+  </Script>
+</body>
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
