a2a07e600f31f89f9ff65f4ca808ffe34a3edfb0
oscargml <oscargml@gmail.com>
2026-05-27 16:55:04 -0600

    Refactor layout to include Google Analytics scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-<body className="min-h-full flex flex-col">
-  {children}
-
-  <Script
-    src="https://www.googletagmanager.com/gtag/js?id=G-33W8GG195N"
-    strategy="afterInteractive"
-  />
-  <Script id="google-analytics" strategy="afterInteractive">
-    {`
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-33W8GG195N');
-    `}
-  </Script>
-</body>
 import Script from "next/script";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -47,7 +31,22 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-33W8GG195N"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-33W8GG195N');
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
